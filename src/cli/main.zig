4b1985c8f6082858ce82d9ef555294dbc860d750
clonker <1685266+clonker@users.noreply.github.com>
2025-08-08 14:21:22 +0200

    cli reads file

--- src/cli/main.zig
+++ src/cli/main.zig
@@ -1,23 +1,29 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
+const std = @import("std");
 const gyul = @import("gyul");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    if (args.len != 2) {
+        std.debug.print("Needs exactly one argument.\n", .{});
+        return;
+    }
 
-    try bw.flush(); // Don't forget to flush!
+    const filename = args[1];
+    const contents = try std.fs.cwd().readFileAllocOptions(allocator, filename, 1e8, null, @alignOf(u8), 0);
+    defer allocator.free(contents);
+
+    var tokenizer = gyul.GYulTokenizer.init(contents);
+    var currentToken = tokenizer.next();
+    while(currentToken.tag != .invalid and currentToken.tag != .eof) : (currentToken = tokenizer.next()) {
+        tokenizer.dump(&currentToken);
+    }
+    tokenizer.dump(&currentToken);
 }
 
 test "simple test" {
@@ -41,5 +47,3 @@ test "fuzz example" {
     };
     try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
-
-const std = @import("std");
--- src/gyul/root.zig
+++ src/gyul/root.zig
@@ -2,10 +2,4 @@ const std = @import("std");
 const tokenizer = @import("tokenizer.zig");
 const testing = std.testing;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+pub const GYulTokenizer = tokenizer.GYulTokenizer;
